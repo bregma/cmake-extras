5523263551ca4b9f02545e93884dd06200a9ac04
Chris Gagnon <chris.gagnon@canonical.com>
2014-05-28 05:48:55 -0400

    lowercase Lcov, clean up whitespace

--- EnableCoverageReport.cmake
+++ EnableCoverageReport.cmake
@@ -36,11 +36,11 @@
 #   CoR-Lab, Research Institute for Cognition and Robotics
 #     Bielefeld University
 
-FIND_PACKAGE(Lcov)
+FIND_PACKAGE(lcov)
 FIND_PACKAGE(gcovr)
 
 FUNCTION(ENABLE_COVERAGE_REPORT)
-    
+
     # argument parsing
     SET(MULTI_VALUE_ARGS FILTER TARGETS TESTS)
     CMAKE_PARSE_ARGUMENTS(ENABLE_COVERAGE_REPORT "" "" "${MULTI_VALUE_ARGS}" ${ARGN})
@@ -50,7 +50,7 @@ FUNCTION(ENABLE_COVERAGE_REPORT)
     SET(COVERAGE_REPORT_DIR "${CMAKE_BINARY_DIR}/coveragereport")
     SET(COVERAGE_XML_FILE "${CMAKE_BINARY_DIR}/coverage.xml")
     SET(COVERAGE_XML_COMMAND_FILE "${CMAKE_BINARY_DIR}/coverage-xml.cmake")
-    
+
     # decide if there is any tool to create coverage data
     SET(TOOL_FOUND FALSE)
     IF(LCOV_FOUND OR GCOVR_FOUND)
@@ -59,34 +59,34 @@ FUNCTION(ENABLE_COVERAGE_REPORT)
     IF(NOT TOOL_FOUND)
         MESSAGE(STATUS "Cannot enable coverage targets because neither lcov nor gcovr are found.")
     ENDIF()
-    
+
     IF(CMAKE_COMPILER_IS_GNUCXX AND TOOL_FOUND AND "${enable_coverage}")
-    
+
         MESSAGE(STATUS "Coverage support enabled for targets: ${ENABLE_COVERAGE_REPORT_TARGETS}")
-    
+
         # create coverage build type
         SET(CMAKE_CXX_FLAGS_COVERAGE ${CMAKE_CXX_FLAGS_DEBUG} PARENT_SCOPE)
         SET(CMAKE_C_FLAGS_COVERAGE ${CMAKE_C_FLAGS_DEBUG} PARENT_SCOPE)
         SET(CMAKE_CONFIGURATION_TYPES ${CMAKE_CONFIGURATION_TYPES} coverage PARENT_SCOPE)
-    
+
         # instrument targets
         SET_TARGET_PROPERTIES(${ENABLE_COVERAGE_REPORT_TARGETS} PROPERTIES COMPILE_FLAGS --coverage
                                                         LINK_FLAGS --coverage)
-    
+
         # html report
         IF (LCOV_FOUND)
-        
+
             MESSAGE(STATUS "Enabling HTML coverage report")
-    
+
             # set up coverage target
-            
+
             ADD_CUSTOM_COMMAND(OUTPUT ${COVERAGE_RAW_FILE}
                                COMMAND ${LCOV_EXECUTABLE} -c -d ${CMAKE_BINARY_DIR} -o ${COVERAGE_RAW_FILE}
                                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                                COMMENT "Collecting coverage data"
                                DEPENDS ${ENABLE_COVERAGE_REPORT_TARGETS} ${ENABLE_COVERAGE_REPORT_TESTS}
                                VERBATIM)
-            
+
             # filter unwanted stuff
             LIST(LENGTH ENABLE_COVERAGE_REPORT_FILTER FILTER_LENGTH)
             IF(${FILTER_LENGTH} GREATER 0)
@@ -98,7 +98,7 @@ FUNCTION(ENABLE_COVERAGE_REPORT)
             ELSE()
                 SET(FILTER "")
             ENDIF()
-            
+
             ADD_CUSTOM_COMMAND(OUTPUT ${COVERAGE_FILTERED_FILE}
                                COMMAND ${LCOV_EXECUTABLE} -e ${COVERAGE_RAW_FILE} "${CMAKE_SOURCE_DIR}*"  -o ${COVERAGE_FILTERED_FILE}
                                ${FILTER}
@@ -111,34 +111,34 @@ FUNCTION(ENABLE_COVERAGE_REPORT)
                                DEPENDS ${COVERAGE_FILTERED_FILE}
                                COMMENT "Generating HTML coverage report in ${COVERAGE_REPORT_DIR}"
                                VERBATIM)
-                               
+
             ADD_CUSTOM_TARGET(coverage-html
                               DEPENDS ${COVERAGE_REPORT_DIR})
-                              
+
         ENDIF()
-        
+
         # xml coverage report
         IF(GCOVR_FOUND)
-        
+
             MESSAGE(STATUS "Enabling XML coverage report")
-        
+
             # gcovr cannot write directly to a file so the execution needs to
             # be wrapped in a cmake file that generates the file output
             FILE(WRITE ${COVERAGE_XML_COMMAND_FILE}
                  "SET(ENV{LANG} en)\n")
             FILE(APPEND ${COVERAGE_XML_COMMAND_FILE}
                  "EXECUTE_PROCESS(COMMAND \"${GCOVR_EXECUTABLE}\" -x -r \"${CMAKE_SOURCE_DIR}\" OUTPUT_FILE \"${COVERAGE_XML_FILE}\" WORKING_DIRECTORY \"${CMAKE_BINARY_DIR}\")\n")
-        
+
             ADD_CUSTOM_COMMAND(OUTPUT ${COVERAGE_XML_FILE}
                                COMMAND ${CMAKE_COMMAND} ARGS -P ${COVERAGE_XML_COMMAND_FILE}
                                COMMENT "Generating coverage XML report"
                                VERBATIM)
-                               
+
             ADD_CUSTOM_TARGET(coverage-xml
                               DEPENDS ${COVERAGE_XML_FILE})
-        
+
         ENDIF()
-        
+
         # provide a global coverage target executing both steps if available
         SET(GLOBAL_DEPENDS "")
         IF(LCOV_FOUND)
@@ -151,7 +151,7 @@ FUNCTION(ENABLE_COVERAGE_REPORT)
             ADD_CUSTOM_TARGET(coverage
                               DEPENDS ${GLOBAL_DEPENDS})
         ENDIF()
-                          
+
     ENDIF()
 
-ENDFUNCTION()   
+ENDFUNCTION()
